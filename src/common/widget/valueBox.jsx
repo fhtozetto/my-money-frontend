493f52a81a2a9d6fec5465cbdacf59fa8cf9988b
Fernando Henrique Tozetto <fhtozetto@gmail.com>
2019-11-20 17:57:15 -0300

    Usando ValueBox

--- src/common/widget/valueBox.jsx
+++ src/common/widget/valueBox.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import Grid from '../layout/grid'
 
 export default props => (
-    <Grid>
+    <Grid cols={props.cols}>
         <div className={`small-box bg-${props.color}`}>
             <div className='inner'>
                 <h3>{props.value}</h3>
                 <p>{props.text}</p>
             </div>
             <div className='icon'>
-                <i className={`fa fa=${props.icon}`}></i>
+                <i className={`fa fa-${props.icon}`}></i>
             </div>
         </div>
     </Grid>
