53b4be143ea497081d4dafbeb336dae1cc61ac9d
Fernando Henrique Tozetto <fhtozetto@gmail.com>
2020-05-21 08:20:13 -0300

    Exibindo Aba de Alterar Cilco de Pagamento

--- src/billingCycle/billlingCycleList.jsx
+++ src/billingCycle/billlingCycleList.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
-import { getList } from './billingCycleActions'
+import { getList, showUpdate } from './billingCycleActions'
 
 class BillingCyclesList extends Component {
 
@@ -16,6 +16,11 @@ class BillingCyclesList extends Component {
                 <td>{bc.name}</td>
                 <td>{bc.month}</td>
                 <td>{bc.year}</td>
+                <td>
+                    <button className='btn btn-warning' onClick={() => this.props.showUpdate(bc)} >
+                        <i className='fa fa-pencil'></i>
+                    </button>
+                </td>
             </tr>
         ))
     }
@@ -41,5 +46,5 @@ class BillingCyclesList extends Component {
 }
 
 const mapStateToProps = state => ({list: state.billingCycles.list})
-const mapDispatchToProps = dispatch => bindActionCreators({getList}, dispatch)
+const mapDispatchToProps = dispatch => bindActionCreators({getList, showUpdate}, dispatch)
 export default connect(mapStateToProps, mapDispatchToProps)(BillingCyclesList)
