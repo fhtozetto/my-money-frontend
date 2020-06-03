ad9428d31a900faac829b3f13b740b7c529f010d
Fernando Henrique Tozetto <fhtozetto@gmail.com>
2020-06-03 08:11:49 -0300

    Navegacao com React-router versao 2

--- src/main/routes.jsx
+++ src/main/routes.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
-import { Router, Route, Redirect, hashHistory } from 'react-router'
+import { Router, Route, IndexRoute, Redirect, hashHistory } from 'react-router'
 
+import App from './app'
 import Dashboard from '../dashboard/dashboard'
 import BillingCyle from '../billingCycle/billingCycle'
 
 export default props => (
     <Router history={hashHistory}>
-        <Route path='/' component={Dashboard} />
-        <Route path='/billingCycles' component={BillingCyle} />
+        <Route path='/' component={App}>
+            <IndexRoute component={Dashboard} />
+            <Route path='billingCycles' component={BillingCyle} />
+        </Route>
         <Redirect from='*' to='/'/>
     </Router>
 )
