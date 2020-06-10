3903851be24cf78db95c34645013d1e3fe2f4be9
Fernando Henrique Tozetto <fhtozetto@gmail.com>
2020-06-10 00:28:01 -0300

    Atualizacao React 16 e bibliotecas

--- src/main/routes.jsx
+++ src/main/routes.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import { Router, Route, IndexRoute, Redirect, hashHistory } from 'react-router'
+import { Switch, Route, Redirect } from 'react-router'
 
 import AuthOrApp from './authOrApp'
 import Dashboard from '../dashboard/dashboard'
 import BillingCyle from '../billingCycle/billingCycle'
 
 export default props => (
-    <Router history={hashHistory}>
-        <Route path='/' component={AuthOrApp}>
-            <IndexRoute component={Dashboard} />
-            <Route path='billingCycles' component={BillingCyle} />
-        </Route>
-        <Redirect from='*' to='/'/>
-    </Router>
+    <div className='content-wrapper'>
+        <Switch>
+            <Route exact path='/' component={Dashboard} />
+            <Route path='/billingCycles' component={BillingCyle} />
+            <Redirect from='*' to='/' />
+        </Switch>
+    </div>
 )
