3fc012126f25e191710e4ed330b4d66a562d2367
Fernando Henrique Tozetto <fhtozetto@gmail.com>
2020-06-14 20:54:22 -0300

    correcao de grafia

--- src/common/template/footer.jsx
+++ src/common/template/footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default props => (
     <footer className='main-footer'>
         <strong>
-            Cuso da <a href='https://www.cod3r.com.br/' target='_blank'>Cod3r </a>
+            Curso da <a href='https://www.cod3r.com.br/' target='_blank'>Cod3r </a>
             - Aluno  
             <a href='https://github.com/fhtozetto/' target='_blank'> Fernando Henrique Tozetto </a>.
         </strong>
